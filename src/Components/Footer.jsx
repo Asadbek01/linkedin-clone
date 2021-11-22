30b571ae30ef548fdf3678b6f6dbe6438c610f05
yonatanski <phpcodeyg@gmail.com>
2021-11-22 12:06:59 +0100

    Starter

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,42 @@
+import { Container } from 'react-bootstrap';
+import React from 'react';
+import {Footerr} from "./Asset/Footerr.css"
+
+
+
+
 const Footer = () => {
     return (
-        <h2>Footer</h2>
+        <>
+            <Container className="profile_page footer_Big">
+                <div className="d-none d-sm-inline footer_box_1">
+                    <img height="25px"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png" />
+                    <ui>
+                        <li>About</li>
+                        <li>Accessibility</li>
+                        <li>Help Center</li>
+                        <li>About</li>
+                    </ui>
+                    <p>LinkedIn Corporation © 2021</p>
+                </div>
+
+                <div className=" d-none d-md-inline footer_box_3">
+                    <ui>
+                        <li>Talent Solutions</li>
+                        <li>Marketing Solutions</li>
+                        <li>Advertising</li>
+                        <li>Small Business</li>
+                    </ui>
+                </div>
+
+         
+
+            </Container>
+
+        </>
+
+
     )
 }
 
