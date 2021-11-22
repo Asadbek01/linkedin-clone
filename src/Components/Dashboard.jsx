0775641475466499f35879df5b467e0224935a02
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-22 15:30:56 +0000

    adding dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,6 +1,31 @@
+import ListGroup from 'react-bootstrap/ListGroup'
+
 const Dashboard = () => {
     return (
-        <h3>Dashboard</h3>
+        <div className="dashboard p-4">
+            <h5>Your Dashboard</h5>
+            <p className='text-muted'>Private To You</p>
+            <ListGroup horizontal className='w-100'>
+                <ListGroup.Item>
+                    <div className="d-flex flex-column">
+                        <p className='dashboard-number mb-1'>18</p>
+                        <p className='mb-1'>Who viewed your profile</p>
+                    </div>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <div className="d-flex flex-column">
+                        <p className='dashboard-number mb-1'>30</p>
+                        <p className='mb-1'>Post Views</p>
+                    </div>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <div className="d-flex flex-column">
+                        <p className='dashboard-number mb-1'>22</p>
+                        <p className='mb-1'>Search Appearances</p>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+        </div>
     )
 }
 
