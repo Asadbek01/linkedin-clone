031419d4085feeda7e8c07a7b685e462220f6ea1
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-22 17:43:14 +0000

    finished the footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Footer from "./Components/Footer"
+import Footer2 from "./Components/Footer2"
 import MyNavbar from "./Components/MyNavbar"
 import Home from "./Pages/Home"
 import NotFound from "./Pages/NotFound"
 import Profile from "./Pages/Profile"
 import ProfileDetails from "./Pages/ProfileDetails"
-import "./Components/Asset/Footerr.css"
-import './Components/Asset/style.css'
+import Jobs from "./Pages/Jobs"
+import Messages from "./Pages/Messages"
+import Network from "./Pages/Network"
+import Notifications from "./Pages/Notifications"
+import "./Footer.css"
+import './style.css'
 
 function App() {
   return (
@@ -15,11 +19,15 @@ function App() {
       <MyNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/network" element={<Network />} />
+        <Route path="/notifications" element={<Notifications />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile/:profileId" element={<ProfileDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      <Footer2 />
     </BrowserRouter>
   )
 }
