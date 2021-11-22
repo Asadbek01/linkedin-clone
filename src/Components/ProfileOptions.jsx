bd979d67092af47e6ccb4e37001cda0271ea0db6
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-22 20:50:44 +0000

    profile options

---
+++ src/Components/ProfileOptions.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+const ProfileOptions = () => {
+    return (
+        <div className='sidebar-wrapper'>
+        <Link to="/">
+          <div className="py-3 text-dark d-flex justify-content-between">
+            <p className='mb-0'>Update your public profile and URL{" "}</p>
+            <i class="bi bi-question-circle-fill"></i>
+          </div>
+        </Link>
+
+        <hr className='my-1'/>
+
+        <Link to="/">
+            <div className="py-3 text-dark d-flex justify-content-between">
+                <p className='mb-0'>Add your profile in another language{" "}</p>
+                <i class="bi bi-question-circle-fill"></i>
+            </div>
+        </Link>
+        
+      </div>
+    )
+}
+
+export default ProfileOptions
