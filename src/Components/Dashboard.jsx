91fce16a87f783d7901c565af78b5dd971c6cc23
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-22 15:58:11 +0000

    made dashboard into reusable components

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,34 +1,20 @@
 import ListGroup from 'react-bootstrap/ListGroup'
 import DashboardItem from './DashboardItem'
+import DashboardRowItem from './DashboardRowItem'
 
 const Dashboard = () => {
 
     return (
         <div className="dashboard p-4">
-            <h5>Your Dashboard</h5>
+            <h5 className='dashboard-heading'>Your Dashboard</h5>
             <p className='text-muted'>Private To You</p>
-            <ListGroup horizontal className='w-100'>
-                <ListGroup.Item>
-                    <div className="d-flex flex-column">
-                        <p className='dashboard-number mb-1'>18</p>
-                        <p className='mb-1'>Who viewed your profile</p>
-                    </div>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <div className="d-flex flex-column">
-                        <p className='dashboard-number mb-1'>30</p>
-                        <p className='mb-1'>Post Views</p>
-                    </div>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <div className="d-flex flex-column">
-                        <p className='dashboard-number mb-1'>22</p>
-                        <p className='mb-1'>Search Appearances</p>
-                    </div>
-                </ListGroup.Item>
+            <ListGroup horizontal className='w-100 rounded'>
+                <DashboardRowItem number='18' text='Who viewed your profile'/>
+                <DashboardRowItem number='30' text='Post Views'/>
+                <DashboardRowItem number='22' text='Search Appearances'/>
             </ListGroup>
 
-            <ListGroup className='w-100 mt-4'>
+            <ListGroup className='w-100 mt-4 rounded'>
                 <DashboardItem title='Creator Mode' subtitle='Grow your audience and get discovered by highlighting content on your profile' icon='bi bi-megaphone-fill mr-3' />
                 <DashboardItem title='My Network' subtitle='Manage your connections, events and interests' icon='bi bi-people-fill mr-3' />
                 <DashboardItem title='Salary Insights' subtitle='See how your salary compares to others in the community' icon='bi bi-cash mr-3' />
