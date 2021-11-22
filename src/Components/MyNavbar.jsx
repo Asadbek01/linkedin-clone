381bf43dace0a80ae9e407b43f09dd6585727b4b
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-11-22 17:19:52 +0100

    added links

--- src/Components/MyNavbar.jsx
+++ src/Components/MyNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Container,NavDropdown, InputGroup } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Container,NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { BsLinkedin, BsHouseDoorFill, BsFillChatDotsFill,BsSearch, BsFillBriefcaseFill,BsBellFill } from 'react-icons/bs'
 
@@ -11,23 +11,38 @@ const MyNavbar = () => {
 			<Link to="/*" className='navbar-brand pb-2 font-weight-bold'>
 			 <BsLinkedin size='25px' />
 			</Link>
-			<InputGroup className="mb-3">
-    <InputGroup.Prepend>
-	<BsSearch />  
-	  </InputGroup.Prepend>
-	<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-  </InputGroup>
-	
-		
+		<Form inline>
+		  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+		</Form>
+		{/* <BsSearch /> */}
 	  <Navbar.Toggle aria-controls="basic-navbar-nav" />
 	  <Navbar.Collapse id="basic-navbar-nav">
-		<Nav className="mr-auto ">
-		  <Nav.Link href="#home"><BsHouseDoorFill size='20px' className="ml-5"/></Nav.Link>
-		  <Nav.Link href="#link">< BsFillChatDotsFill size='20px'/></Nav.Link>
-		  <Nav.Link href="#link">< BsFillBriefcaseFill size='20px'/></Nav.Link>
-		  <Nav.Link href="#link">< BsFillChatDotsFill size='20px'/></Nav.Link>
-		  <Nav.Link href="#link">< BsBellFill  size='20px'/></Nav.Link>
-		  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+		<Nav className="mr-auto icons " >
+			<Link to='/home'>
+		  <div  className='nav-link' ><BsHouseDoorFill size='20px ' className="mr-2 "/></div> 
+		  <p className='Home'>Home</p>
+			 </Link>
+			 <Link to='/mynetwork'>
+		  <div className='nav-link' >< BsFillChatDotsFill size='20px' className='mr-2'/></div>
+		  <p className='mynetwork'>MyNetwork</p>
+			 </Link>
+			 <Link to='/jobs'>
+		  <div className='nav-link' >< BsFillBriefcaseFill size='20px' className='mr-2'/></div> 
+			 <p className='Jobs'>Jobs</p>
+			 </Link>
+			 <Link to='/messaging'>
+		  <div className='nav-link' >< BsFillChatDotsFill size='20px' className='mr-2'/></div> 
+		  <p className='messaging'>Messaging</p>
+			 </Link>
+			 <Link to='/notification'>
+		  <div className='nav-link'>< BsBellFill  size='20px' className='mr-2'/></div> 
+			 <p className='notification'>Notification</p>
+
+			 </Link>
+
+
+		  <NavDropdown title='Me' id="basic-nav-dropdown" >
+			  
 			  <p>Lorem ipsum dolor sit amet consectetur 
 				  <br /> adipisicing elit. Nemo repudiandae.</p>
 			<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
