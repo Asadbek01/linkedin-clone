be9403a4b6ba8c6e6c86867633c935d54f3494ca
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-23 13:58:07 +0100

    structured

---
+++ src/Components/Licence.jsx
@@ -0,0 +1,63 @@
+import { Row, Card } from "react-bootstrap";
+
+  const Licence =()=>{
+
+return (
+  <div className="profile-sub-section mt-4">
+    <h4 className="pt-4 pl-3">Licenses & certifications</h4>
+    <i className="bi bi-plus mt-3 mr-4"></i>
+    <i
+      className="bi bi-pencil p-2"
+      onClick={(e) => console.log(e)}
+      //   {handleShowModal}
+    ></i>
+    <Row className="p-3">
+      <Card className="mb-3 profile-cards">
+        <div className="d-flex align-items-center">
+          {/* <Card.Img src={image} className="sidebar-profile-images ml-2" /> */}
+          <Card.Body className="p-2">
+            <div className="d-flex justify-content-between">
+              <Card.Title className="mb-1 text-dark">
+                Learn Javascript Course
+              </Card.Title>
+              <i
+                className="bi bi-pencil p-2"
+                onClick={(e) => console.log(e)}
+                //   {handleShowModal}
+              ></i>
+            </div>
+            <Card.Text className="text-muted">StriveSchool</Card.Text>
+            <Card.Text className="text-muted">
+              Issued Sep 2021 - No Exipration Date
+            </Card.Text>
+          </Card.Body>
+        </div>
+      </Card>
+
+      <Card className="mb-3 profile-cards">
+        <div className="d-flex align-items-center">
+          {/* <Card.Img src={image} className="sidebar-profile-images ml-2" /> */}
+          <Card.Body className="p-2">
+            <div className="d-flex justify-content-between">
+              <Card.Title className="mb-1 text-dark">
+                Learn Javascript Course
+              </Card.Title>
+              <i
+                className="bi bi-pencil p-2"
+                onClick={(e) => console.log(e)}
+                //   {handleShowModal}
+              ></i>
+            </div>
+            <Card.Text className="text-muted">StriveSchool</Card.Text>
+            <Card.Text className="text-muted">
+              Issued Sep 2021 - No Exipration Date
+            </Card.Text>
+          </Card.Body>
+        </div>
+      </Card>
+    </Row>
+  </div>
+);
+};
+
+export default Licence;
