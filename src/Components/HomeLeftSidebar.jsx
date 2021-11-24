b2469e60b18703ebfd3f32e7fcf727348250fecf
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-11-24 12:13:36 +0100

    Added img and styles

--- src/Components/HomeLeftSidebar.jsx
+++ src/Components/HomeLeftSidebar.jsx
@@ -1,6 +1,21 @@
+import {Card} from 'react-bootstrap';
+import Image from '../assets/Asadbek.png'
 const HomeLeftSidebar = () => {
     return (
-        <h2>HomeLeftSidebar</h2>
+        <Card
+        style={{ width: '17rem', height: '10rem', }}
+            className="mb-1"
+                          >
+                   <Card.Header className='text-center'>
+                   <img className='sidebar_img' src={Image} alt="jbdjkb" />
+                   </Card.Header>
+                   <Card.Body className=''>
+                       <Card.Title className='text-center'> Asadbek Azamjonov </Card.Title>
+                       <Card.Text className='text-secondary text-center'>
+                       <p >Bachelor's degree at Akademia Finansów i Biznesu Vistula</p>
+                       </Card.Text>
+                   </Card.Body>
+               </Card>
     )
 }
 
