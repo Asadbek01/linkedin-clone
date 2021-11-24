f2217b2d68c78e39d3a18ed573d259d115f643b3
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-11-24 16:54:42 +0100

    added some features

--- src/Components/HomeLeftSidebar.jsx
+++ src/Components/HomeLeftSidebar.jsx
@@ -5,9 +5,9 @@ const HomeLeftSidebar = () => {
     return (
         <>
        
-               <Card style={{ width: '18rem' }}>
+               <Card style={{ width: '20rem' }}>
               <Card.Header  className='text-center'  style={{height: '4rem', }}>
-                   <img style={{width: "75px"}} src={Image} alt="jbdjkb" />
+                   <img className='sidebar-image' src={Image} alt="jbdjkb" />
                    </Card.Header>
                    <Card.Title className='text-center mt-5'> Asadbek Azamjonov </Card.Title>
                    <Card.Text className='text-secondary text-center'>
@@ -15,42 +15,52 @@ const HomeLeftSidebar = () => {
                        </Card.Text>
                <ListGroup variant="flush" className='d-flex'>
                    
-                 <a className='text_muted' className='pl-3 pt-2'>Who viewed your profile</a>
-                 <a className='textmuted' className='pl-3 pt-2'>Who viewed your profile</a>
+                <div classNamed ='d-flex'>
+                <p className='pl-3 pt-1 mt-2 text_muted'>Who viewed your profile</p>
+                <p className='num_prof'>18</p>
+                </div>
+                <div classNamed ='d-flex'>
+               <p className='pl-3 pt-1 text_muted '>View of your post</p></div>
+               <p className='num_post'>30</p>
                </ListGroup>
-               <ListGroup variant="flush" className='d-flex'>
-                 <a className='pl-3 pt-1 pb-1'>Access exclusive tools & insights</a>
-                 <a className='pl-3 pt-1 pb-1'>Try Premium for free</a>
+               <ListGroup variant="flush">
+               <div classNamed ='d-flex'>
+   
+                 <p className='pl-3 pt-1 mt-2  text_muted'>Access exclusive tools & insights</p>
+                 <p className='pl-5 pt-1  text-dark'>Try Premium for free</p></div>
                </ListGroup>
                <ListGroup variant='flush'>
-               <p className='pl-3 pt-2'> <BsBookmarkFill />My items</p>
+               <div className='d-flex'><BsBookmarkFill className='saved'/> <p className='pl-2'> My items</p></div>
                </ListGroup>
              </Card>
-          <Card  className="mt-2"style={{ width: '18rem', }}>
+          <Card  className="mt-2"style={{ width: '20rem', }}>
           <Card.Body>
             <Card.Title>Recent</Card.Title>
 <ListGroup>
-    <p><BsPeopleFill/>Intership Japan</p>
-    <p><BsPeopleFill/>React Developer-ReactJS...</p>
-    <p><BsPeopleFill/>Angular, React, Vue, Node, I...</p>
-    <p><BsPeopleFill/>UI/UX User Experience Inter...</p>
-    <p><BsPeopleFill/>iOS Developer Group</p>
+
+ <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>Intership Japan</p> </div>
+ <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>React Developer-ReactJS...</p></div>
+ <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>Angular, React, Vue, Node, I...</p></div>
+ <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>UI/UX User Experience Inter...</p></div>
+ <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>iOS Developer Group</p></div>
+   {/* </div> */}
     </ListGroup>  
     <Card.Title>Groups</Card.Title>
     <ListGroup>
-    <p><BsPeopleFill/>Intership Japan</p>
-    <p><BsPeopleFill/>React Developer-ReactJS...</p>
-    <p><BsPeopleFill/>Angular, React, Vue, Node, I...</p>
+    <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>Intership Japan</p></div>
+    <div className='d-flex'><BsPeopleFill/><p className='text_muted pl-2'>React Developer-ReactJS...</p></div>
+    <div className='d-flex'><BsPeopleFill/> <p className='text_muted pl-2'>Angular, React, Vue, Node, I...</p></div>
     </ListGroup>
     <div>
-    <a href="">Events</a>
-    <a href="">Followed Hashtags</a>
+    <a href="" >Events</a> <br />
+    <a  href="">Followed Hashtags</a>
     </div>
-    <ListGroup variant='flush'>
-  <p>Discover more</p>
-    </ListGroup>
+   
       
           </Card.Body>
+    <ListGroup variant='flush'>
+     <p className='m-auto p-2' >Discover more</p>
+    </ListGroup>
         </Card> 
           </>  
           
