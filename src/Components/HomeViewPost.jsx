69d04f91a27198eb5833ac4c5e5712f41cc87a87
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-25 09:59:34 +0000

    able to delete your own post

--- src/Components/HomeViewPost.jsx
+++ src/Components/HomeViewPost.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import SinglePost from './SinglePost'
 import SkeletonPost from './SkeletonPost'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
 const HomeViewPost = ({ postsAdded }) => {
 
@@ -8,6 +10,57 @@ const HomeViewPost = ({ postsAdded }) => {
     const [isLoading, setIsLoading] = useState(true)
     const [randomNumber, setRandomNumber] = useState(0)
     const [numOfPosts, setNumOfPosts] = useState(25)
+
+    const [postsChanged, setPostsChanged] = useState(0)
+    const [selectedPost, setSelectedPost] = useState(null)
+    const [selectedPostDetails, setSelectedPostDetails] = useState(null)
+    const [showModal, setShowModal] = useState(false)
+    const handleCloseModal = () => setShowModal(false)
+    const handleShowModal = () => setShowModal(true)
+
+    const fetchPostDetails = async () => {
+        try {
+            const response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${selectedPost}`, {
+                headers: {
+                    'Authorization': process.env.REACT_APP_TOKEN
+                }
+            })
+            if (response.ok) {  
+                const data = await response.json()
+                setSelectedPostDetails(data)
+            } else {
+                console.error('Fetch Failed')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchPostDetails()
+    }, [selectedPost])
+
+    const handleEdit = (id) => {
+        setSelectedPost(id)
+        handleShowModal()
+    }
+
+    const handleDeletePost = async () => {
+        try {
+            const response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${selectedPost}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': process.env.REACT_APP_TOKEN
+                }
+            })
+            if (response.ok) {
+                setPostsChanged(count => count + 1)
+                handleCloseModal()
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     const fetchPosts = async () => {
         try {
@@ -33,7 +86,7 @@ const HomeViewPost = ({ postsAdded }) => {
     useEffect(() => {
         fetchPosts()
         // eslint-disable-next-line
-    }, [postsAdded])
+    }, [postsAdded, postsChanged])
 
     return (
         <>
@@ -43,9 +96,24 @@ const HomeViewPost = ({ postsAdded }) => {
         }
         {
             posts &&
-                posts.slice(posts.length - numOfPosts, posts.length).reverse().map(post => <SinglePost post={post} /> )
+                posts.slice(posts.length - numOfPosts, posts.length).reverse().map(post => <SinglePost post={post} handleEdit={handleEdit}/> )
         }
         { posts && <div className="d-flex justify-content-center mt-3"><button className='button main-btn-outline' onClick={() => setNumOfPosts(count => count + 25)} >Show More </button></div> }
+
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+            <Modal.Title>{selectedPostDetails?.username}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{selectedPostDetails?.text}</Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+                Close
+            </Button>
+            <Button variant="danger" onClick={handleDeletePost}>
+                Delete
+            </Button>
+            </Modal.Footer>
+        </Modal>
 
         </>
     )
--- src/Components/SinglePost.jsx
+++ src/Components/SinglePost.jsx
@@ -3,42 +3,9 @@ import Col from 'react-bootstrap/Col'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { parseISO, differenceInHours  } from "date-fns"
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
 
-
-const SinglePost = ({ post }) => {
-
-    const [selectedPost, setSelectedPost] = useState(null)
-    const [selectedPostDetails, setSelectedPostDetails] = useState(null)
-    const [showModal, setShowModal] = useState(false)
-    const handleCloseModal = () => setShowModal(false)
-    const handleShowModal = () => setShowModal(true)
-
-    const fetchPostDetails = async () => {
-        try {
-            const response = await fetch(`https://striveschool-api.herokuapp.com/api/posts/${selectedPost}`, {
-                headers: {
-                    'Authorization': process.env.REACT_APP_TOKEN
-                }
-            })
-            if (response.ok) {  
-                const data = await response.json()
-                setSelectedPostDetails(data)
-            } else {
-                console.error('Fetch Failed')
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    useEffect(() => {
-        fetchPostDetails()
-    }, [])
-
+const SinglePost = ({ post, handleEdit }) => {
 
     return (
         <div className="profile-sub-section p-2 my-2">
@@ -51,10 +18,7 @@ const SinglePost = ({ post }) => {
                         <p className='text-muted mb-0'>{differenceInHours(new Date(), parseISO(post.createdAt))}h &#8226; <i className='bi bi-globe2'></i></p>
                     </div>
                 </div>
-                <i className='bi bi-three-dots' onClick={() => {
-                    handleShowModal()
-                    setSelectedPost(post._id)
-                }}></i>
+                <i className='bi bi-three-dots' onClick={() => handleEdit(post._id)}></i>
             </Row>
             <p className='pl-2 mt-2 mb-2'>{post.text}</p>
             <hr className='my-1' />
@@ -77,37 +41,6 @@ const SinglePost = ({ post }) => {
                 </Col>
             </Row>
 
-            
-              <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                <Modal.Title>{selectedPostDetails?.username}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{selectedPostDetails?.text}</Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseModal}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleCloseModal}>
-                    Save Changes
-                </Button>
-                </Modal.Footer>
-            </Modal>
-
-            
-              <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                <Modal.Title>{selectedPost}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseModal}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleCloseModal}>
-                    Save Changes
-                </Button>
-                </Modal.Footer>
-            </Modal>
         </div>
     )
 }
