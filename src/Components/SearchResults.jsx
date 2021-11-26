5d15834c8fa4f216ecd36dd924df26e712452740
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-26 11:15:59 +0000

    added basic search page

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,4 +1,4 @@
-import { ListGroup } from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import SingleSearchResult from "./SingleSearchResults"
@@ -35,11 +35,19 @@ const SearchResults = ({ heading, type }) => {
     return (
         <div className="profile-sub-section p-3 my-2">
             <h2 className=''>{heading}</h2>
-            <ListGroup>
-                {
-                    data && data.slice(0, 3).map(d => <SingleSearchResult data={d} /> )
+            <Row>
+                <Col xs='12' >
+                { 
+                    type === 'profile' ? 
+                    data && data.filter(d => (d.name.toLowerCase() || d.surname.toLowerCase() || d.bio.toLowerCase()).includes(searchQuery.toLowerCase())).slice(0, 3).map(d => <SingleSearchResult data={d} type={type} /> )
+                    :
+                    data && data.filter(d => (d.text.toLowerCase()).includes(searchQuery.toLowerCase())).slice(0, 3).map(d => <SingleSearchResult data={d} type={type} /> )
                 }
-            </ListGroup>
+                {/* {
+                    (data && type ==='posts') && data.filter(d => d.text.toLowerCase().includes(searchQuery.toLowerCase())).slice(0, 3).map(d => <SingleSearchResult data={d} type={type} /> )
+                } */}
+                </Col>
+            </Row>
         </div>
     )
 }
