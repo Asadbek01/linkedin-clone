79fed7f1d24c470e1f5d48afdc27be8f4eb7357e
yonatanski <phpcodeyg@gmail.com>
2021-11-26 12:59:17 +0100

    minor changes

--- src/Components/NetworkFirstPart.jsx
+++ src/Components/NetworkFirstPart.jsx
@@ -13,7 +13,7 @@ import MyButton from "./MyButton"
 const NetworkFirstPart = () => {
   return (
     <Container className="profile-sub-section pt-0 pb-0">
-      <Row className=" pl-1 ">
+      <Row className=" px-2  mt-3">
         <Col>
           <p className=" pl-3 text-muted">Invitations </p>
         </Col>
@@ -21,14 +21,14 @@ const NetworkFirstPart = () => {
           <p className=" text-muted ml-auto ">Manage</p>
         </Col>
       </Row>
-      <hr />
+      <hr className="mt-0 " />
       <Row>
-        <div className="d-flex align-items-center ml-3 mb-2 mt-0">
+        <div className="d-flex align-items-center ml-3 mb-2 mt-0 px-2">
           <img
             src="https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-            className="sidebar-Home2-images ml-2 mt-0 d-flex align-items-baseline"
+            className="sidebar-Home2-images ml-0 mt-0 d-flex align-items-baseline "
           />
-          <div>
+          <div className="">
             <small className="text-muted ml-2">Newsletter.Weekly</small>
             <p className="mb-1 ml-2 text-dark">
               <strong> Dominic McGregor</strong> invited you to Subscribe to{" "}
